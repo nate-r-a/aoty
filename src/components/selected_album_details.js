f7a36b35cf39e1481402080f5d72b2db111b4b47
Nate Rudnick-Aldridge <nate-r-a@Nates-MacBook-Pro.local>
2018-03-13 03:57:57 -0500

    Animations looking pretty good for title/artist and spotify panel

--- src/components/selected_album_details.js
+++ src/components/selected_album_details.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReactCSSTransitionReplace from 'react-css-transition-replace';
 
 const SelectedAlbumDetails = ({album}) => {
   if (!album) {
@@ -8,15 +9,40 @@ const SelectedAlbumDetails = ({album}) => {
   return (
     <div className="selected-album-details">
       <div className="col-md-7 text-details">
-        <div className="artist">{album.artist}</div>
-        <hr/>
-        <div className="title">{album.title}</div>
+        <ReactCSSTransitionReplace transitionName="fade-wait"
+                                     transitionLeaveTimeout={600} transitionenterTimeout={600}>
+          <TextDetails key={album.title} album={album} />
+        </ReactCSSTransitionReplace>
       </div>
       <div className="col-md-5 spotify">
-        <iframe src={"https://open.spotify.com/embed?uri=" + album.uri} width="252" height="460" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+        <ReactCSSTransitionReplace transitionName="fade-wait"
+          transitionLeaveTimeout={875} transitionEnterTimeout={875}>
+
+          <Spotify key={album.uri} album={album} />
+        </ReactCSSTransitionReplace>
       </div>
     </div>
   )
 }
+
+// Testing out splitting this into separate components
+const TextDetails = ({album}) => {
+  return (
+    <div>
+      <div className="artist">{album.artist}</div>
+      <hr />
+      <div className="title">{album.title}</div>
+    </div>
+  )
+}
+
+const Spotify = ({album}) => {
+  return (
+    <iframe src={"https://open.spotify.com/embed?uri=" + album.uri} width="252" height="460" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+  )
+}
+
+
+
 
 export default SelectedAlbumDetails;
